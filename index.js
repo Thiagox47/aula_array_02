bc3267ca6fa108c7ca1835c75a54b71ddf72a09f
Wylddyson Galvão <wylddyson.contato@gmail.com>
2022-08-01 22:33:24 -0300

    Aula 02

---
+++ index.js
@@ -0,0 +1,67 @@
+(() => {
+    const array = [
+        "Wylddyson",
+        "André",
+        "Miguel"
+    ];
+    const teste = "jose";
+
+    let data = [
+        {
+            "id":1,
+            "name":"André",
+            "lastname":"Brandão",
+            "age":26,
+            "gender":"Male"
+        },
+        {
+            "id":2,
+            "name":"Paulo",
+            "lastname":"Daniel",
+            "age":20,
+            "gender":"Malee"
+        },
+        {
+            "id":3,
+            "name":"José",
+            "lastname":"Galvão",
+            "age":29,
+            "gender":"Male"
+        },
+        {
+            "id":4,
+            "name":"Luana",
+            "lastname":"Silva",
+            "age":21,
+            "gender":"Female"
+        },
+
+    ];
+
+    
+    // data.push({
+    //     "id":5,
+    //     "name":"Bruno",
+    //     "lastname":"Bezerra",
+    //     "age":18,
+    //     "gender":"undefined"
+    // })
+
+
+    // const find = data.map((item) =>{
+    //      if(item.id === 1){
+    //         return item;
+    //      }
+    // })
+ 
+    // const find_filter = data.filter(item => item.id === 1 );
+
+    // data.map(item =>{
+    //     item.age = 13;
+    // })
+
+    // data.pop()
+
+    console.log(data.reverse())
+   
+})();
